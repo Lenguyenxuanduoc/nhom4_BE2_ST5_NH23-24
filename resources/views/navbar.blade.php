00d61c8234d6cd9b1ff84eee1a0971a61ee78012
Mai Nguyen Hoang Vu <mainguyenhoangvu2212@gmail.com>
2024-04-11 01:45:13 +0700

    Thêm giao diện

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,25 @@
+ <!------- Start navbar ------->
+ <nav>
+    <div class="container nav-container">
+        <a href="home" class="logo"><h3>Exotic Cars.</h3></a>
+        <ul class="nav-link">
+            <li><a href="home" class="active">Home</a></li>
+            <li><a href="news">News</a></li>
+            <li><a href="">Cars</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+            <li>
+                <form action="" class="search">
+                    <input type="text" name="tag" placeholder="Search">
+                    <button type="submit" style="display:none"></button>
+                </form>
+            </li>
+        </ul>
+        <ul class="social-link">
+            <li><a href=""><i class="fa-brands fa-facebook"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-instagram"></i></a></li>
+            <li><a href=""><i class="fa-brands fa-x-twitter"></i></a></li>
+        </ul>
+    </div>
+</nav>
+<!------- End navbar ------->
